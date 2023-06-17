31aa4d574cf817247f68c2978393c994194f52c2
Marco Bobr <marrcokyr@gmail.com>
2023-06-17 17:58:24 +0300

    tf is wrong with env

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+
   getHello(): string {
-    console.log(process.env.NODE_ENV)
-    return process.env.NODE_ENV;
+    return process.env.PRIVATE_API_KEY_MAILGUN;
   }
 }
